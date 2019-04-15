0c55c9e086a2837cb451115240fe583ebd39c369
cyfotyasi <Tya@2017>
2019-04-16 00:57:32 +0200

    commit first project

--- src/main/java/za/ac/cputassignment/domain/eventTriggs/ArletInfor.java
+++ src/main/java/za/ac/cputassignment/domain/eventTriggs/ArletInfor.java
@@ -1,7 +1,11 @@
 package za.ac.cputassignment.domain.eventTriggs;
 
+import java.util.Date;
+
 public class ArletInfor {
 
+    private String blue,red,greed;
+    private Date simpledate;
 
 
 
--- src/main/java/za/ac/cputassignment/domain/eventTriggs/Notification.java
+++ src/main/java/za/ac/cputassignment/domain/eventTriggs/Notification.java
@@ -1,4 +1,18 @@
 package za.ac.cputassignment.domain.eventTriggs;
 
-public class Notification {
+import za.ac.cputassignment.factory.NotificationFactory;
+
+import java.util.Date;
+
+public class Notification  {
+
+
+
+    private Notification(){}
+
+
+
+
+
+
 }
