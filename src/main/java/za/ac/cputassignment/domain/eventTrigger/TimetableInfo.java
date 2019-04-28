31663b978ba8d78399d04e07d10a876e369be0f0
cyfotyasi <Tya@2017>
2019-04-28 22:19:06 +0200

    commit first project

--- src/main/java/za/ac/cputassignment/domain/eventTrigger/TimetableInfo.java
+++ src/main/java/za/ac/cputassignment/domain/eventTrigger/TimetableInfo.java
@@ -4,14 +4,71 @@ import za.ac.cputassignment.domain.location.Location;
 import za.ac.cputassignment.domain.transport.Vehicle;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class TimetableInfo {
 
     private Date simple;
     private Location location;
     private Vehicle vehicle;
+    private int timetableid;
+
+
+    private TimetableInfo(){}
+
+    private TimetableInfo(Builder builder)
+    {
+        this.timetableid =builder.timetableid;
+    }
+
+    public Date getSimple() {
+        return simple;
+    }
+
+    public int getTimetableid() {
+        return timetableid;
+    }
+
+    public static class Builder{
+
+        private Date simple;
+        private Location location;
+        private Vehicle vehicle;
+        private int timetableid;
+
+        public  Builder timetableId(int timetableid)
+        {
+            this.timetableid = timetableid;
+            return this;
+        }
+
+        public TimetableInfo build()
+        {
+            return new TimetableInfo(this);
+        }
 
 
 
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TimetableInfo that = (TimetableInfo) o;
+        return timetableid == that.timetableid;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(timetableid);
+    }
+
+    @Override
+    public String toString() {
+        return "TimetableInfo{" +
+                "simple=" + simple +
+                ", timetableid=" + timetableid +
+                '}';
+    }
 }
--- src/main/java/za/ac/cputassignment/domain/person/StudentInfo.java
+++ src/main/java/za/ac/cputassignment/domain/person/StudentInfo.java
@@ -1,4 +1,74 @@
 package za.ac.cputassignment.domain.person;
 
+import java.util.Objects;
+
 public class StudentInfo  {
+
+    private String address;
+    private int infid;
+
+
+    private StudentInfo(){}
+
+    private StudentInfo(Builder builder)
+    {
+        this.infid =builder.infid;
+        this.address =builder.address;
+
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getInfid() {
+        return infid;
+    }
+
+    public static class Builder{
+
+        private String address;
+        private int infid;
+
+
+        public  Builder infid(int infid)
+        {
+            this.infid =infid;
+            return this;
+        }
+
+        public  Builder address(String address)
+        {
+            this.address =address;
+            return  this;
+        }
+
+        public StudentInfo build()
+        {
+            return new StudentInfo(this);
+        }
+
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StudentInfo that = (StudentInfo) o;
+        return infid == that.infid;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(infid);
+    }
+
+    @Override
+    public String toString() {
+        return "StudentInfo{" +
+                "address='" + address + '\'' +
+                ", infid=" + infid +
+                '}';
+    }
 }
