c470fe310a303a09cf136c817884f052576a3048
cyfotyasi <Tya@2017>
2019-04-29 13:40:53 +0200

    commit first project

--- src/main/java/za/ac/cputassignment/Repository/LocationRepository/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package za.ac.cputassignment.Repository.LocationRepository;
-
-import za.ac.cputassignment.Repository.MainInterface.IRepository;
-import za.ac.cputassignment.domain.location.Location;
-
-public interface LocationRepository extends IRepository<Location,String> {
-}
