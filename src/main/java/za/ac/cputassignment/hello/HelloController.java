ae16d660819083ab59045fd6fc2888c55caa6124
cyfotyasi <Tya@2017>
2019-05-07 00:48:24 +0200

    commit first project

--- src/main/java/za/ac/cputassignment/hello/HelloController.java
+++ src/main/java/za/ac/cputassignment/hello/HelloController.java
@@ -1,10 +1,15 @@
 package za.ac.cputassignment.hello;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    
+    @RequestMapping("/hello")
+    public String sayHI()
+    {
+        return "Hi";
+    }
 
 }
