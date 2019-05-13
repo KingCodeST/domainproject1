c41de0b0db68cf2adb7e0479fe5498eff1e7a228
cyfotyasi <Tya@2017>
2019-05-13 16:59:12 +0200

    commit first project

---
+++ src/main/java/za/ac/cputassignment/StudentShuttles.java
@@ -0,0 +1,13 @@
+package za.ac.cputassignment;
+
+import org.springframework.boot.SpringApplication;
+
+public class StudentShuttles {
+
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(StudentShuttles.class, args);
+    }
+
+}
