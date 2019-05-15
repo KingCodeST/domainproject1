7d4546ba9f4c85f98d69f9cbb3c57469a6446dcd
cyfotyasi <Tya@2017>
2019-05-15 18:20:08 +0200

    commit first project

--- src/main/java/za/ac/cputassignment/Controller/locationController/CampusController.java
+++ src/main/java/za/ac/cputassignment/Controller/locationController/CampusController.java
@@ -1,9 +1,17 @@
 package za.ac.cputassignment.Controller.locationController;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import za.ac.cputassignment.service.service.LocationService.CampusService;
 
 @RestController
-@RequestMapping
+@RequestMapping("/Campus")
 public class CampusController {
+
+    @Autowired
+    private CampusService service;
+   
+
+
 }
