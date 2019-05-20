5fd718ff6ef1609c1034b9ce46c1b049552f4365
cyfotyasi <Tya@2017>
2019-05-21 01:55:17 +0200

    commit first project

--- src/test/java/za/ac/cputassignment/Repository/impl/eventTriggerRepositoryImpl/ArletInforRepositoryImplTest.java
+++ src/test/java/za/ac/cputassignment/Repository/impl/eventTriggerRepositoryImpl/ArletInforRepositoryImplTest.java
@@ -1,5 +1,6 @@
 package za.ac.cputassignment.Repository.impl.eventTriggerRepositoryImpl;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -35,27 +36,52 @@ public class ArletInforRepositoryImplTest {
         //this.arletInfor = ArletInforFactory
     }
 
-    @Test
-    public void getRepository() {
-    }
 
     @Test
     public void d_getAll() {
+    Set<ArletInfor> all =this.repository.getAll();
+    System.out.println("In getAll, all ="+all);
+    Assert.assertSame(1,all.size());
     }
 
     @Test
     public void a_create() {
+        ArletInfor created=this.repository.create(this.arletInfor);
+        System.out.println("In create, created ="+created);
+        d_getAll();
+        Assert.assertSame(created,this.arletInfor);
     }
 
     @Test
     public void c_update() {
+        String blue ="";
+        String green="";
+        String red ="";
+        String simpledate="";
+        ArletInfor arletInfor=new ArletInfor.Builder().blue(blue).green(green).red(red).simpledate(simpledate).build();
+        System.out.println("In update, about_to_updated = "+arletInfor);
+        ArletInfor updated =this.repository.update(arletInfor);
+        Assert.assertSame(arletInfor,updated);
+
+
     }
 
     @Test
     public void e_delete() {
+        ArletInfor savedArlet =getSavedArletInfor();
+        this.repository.delete(savedArlet.getId());
+        d_getAll();
     }
 
     @Test
     public void b_read() {
+        ArletInfor savedArt=getSavedArletInfor();
+        System.out.println("In read, arlertId = "+savedArt.getId());
+        ArletInfor read =this.repository.read(savedArt.getId());
+        System.out.println("In read, read = "+read);
+        d_getAll();
+        Assert.assertEquals(savedArt,read);
+
+
     }
 }
