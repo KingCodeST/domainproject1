f8ccf56618823d7c579e8ae8c648bba2da674bf4
cyfotyasi <Tya@2017>
2019-09-22 05:30:44 +0200

    [-] commit new Changes

--- src/main/java/za/ac/cputassignment/domain/eventTrigger/LogEvent.java
+++ src/main/java/za/ac/cputassignment/domain/eventTrigger/LogEvent.java
@@ -1,7 +1,77 @@
 package za.ac.cputassignment.domain.eventTrigger;
 
+import java.util.Objects;
+
 public class LogEvent {
 
     private String email;
+    private String event;
 
+    private LogEvent(){}
+
+    private LogEvent(Builder builder)
+    {
+        this.email =builder.email;
+        this.event =builder.event;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getEvent() {
+        return event;
+    }
+
+    public static class Builder{
+        private String email;
+        private String event;
+
+        public Builder setEmail(String email)
+        {
+            this.email=email;
+            return this;
+        }
+
+        public Builder setEvent(String event)
+        {
+            this.event =event;
+            return this;
+        }
+
+        public Builder copy(LogEvent logEvent)
+        {
+            this.event =logEvent.event;
+            this.email =logEvent.email;
+            return this;
+        }
+
+        public LogEvent build()
+        {
+            return new LogEvent(this);
+        }
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LogEvent logEvent = (LogEvent) o;
+        return email.equals(logEvent.email) &&
+                Objects.equals(event, logEvent.event);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email);
+    }
+
+    @Override
+    public String toString() {
+        return "LogEvent{" +
+                "email='" + email + '\'' +
+                ", event='" + event + '\'' +
+                '}';
+    }
 }
--- src/main/java/za/ac/cputassignment/domain/eventTrigger/LoginToken.java
+++ src/main/java/za/ac/cputassignment/domain/eventTrigger/LoginToken.java
@@ -1,8 +1,78 @@
 package za.ac.cputassignment.domain.eventTrigger;
 
+import java.util.Objects;
+
 public class LoginToken {
 
     private String email;
     private String token;
 
+    private LoginToken(){}
+
+    private LoginToken(Builder builder)
+    {
+        this.email =builder.email;
+        this.token =builder.token;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public static class Builder
+    {
+        private String email;
+        private String token;
+
+        public Builder getEmail(String email){
+            this.email =email;
+            return this;
+        }
+
+        public Builder getToken(String token)
+        {
+            this.token =token;
+            return this;
+        }
+
+        public Builder copy(LoginToken loginToken)
+        {
+            this.token =loginToken.token;
+            this.email =loginToken.email;
+            return this;
+        }
+
+        public LoginToken build()
+        {
+            return new LoginToken(this);
+        }
+
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LoginToken that = (LoginToken) o;
+        return email.equals(that.email) &&
+                Objects.equals(token, that.token);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email);
+    }
+
+    @Override
+    public String toString() {
+        return "LoginToken{" +
+                "email='" + email + '\'' +
+                ", token='" + token + '\'' +
+                '}';
+    }
 }
