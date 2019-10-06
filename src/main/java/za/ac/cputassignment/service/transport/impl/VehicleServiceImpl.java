33d160067edc821f146324775a44cf89b2e11435
cyfotyasi <Tya@2017>
2019-10-06 18:15:34 +0200

    [+] services

--- src/main/java/za/ac/cputassignment/service/transport/impl/VehicleServiceImpl.java
+++ src/main/java/za/ac/cputassignment/service/transport/impl/VehicleServiceImpl.java
@@ -9,40 +9,61 @@ import za.ac.cputassignment.domain.transport.Vehicle;
 import za.ac.cputassignment.service.transport.VehicleService;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 
 public class VehicleServiceImpl implements VehicleService  {
 
+    private static  VehicleService vehicleService= null;
+
+    @Autowired
+    private VehicleRepository vehicleRepository;
+
+    private VehicleServiceImpl() {
+    }
+
+    public static VehicleService getVehicleService(){
+        if (vehicleService ==null) vehicleService=new VehicleServiceImpl();
+        return vehicleService;
+    }
 
     @Override
     public Vehicle retrieveByDesc(String vehicleDesc) {
+        List<Vehicle> vehicles= getAll();
+        for(Vehicle vehicle: vehicles)
+        {
+            if (vehicle.getTypeVehicle().equalsIgnoreCase(vehicleDesc))
+                return  vehicle;
+        }
         return null;
     }
 
     @Override
     public List<Vehicle> getAll() {
-        return null;
+        return this.vehicleRepository.findAll();
     }
 
     @Override
     public Vehicle create(Vehicle vehicle) {
-        return null;
+
+        return this.vehicleRepository.save(vehicle);
     }
 
     @Override
     public Vehicle read(String s) {
-        return null;
+        Optional<Vehicle> optVehicle =this.vehicleRepository.findById(s);
+        return optVehicle.orElse(null);
     }
 
     @Override
     public Vehicle update(Vehicle vehicle) {
-        return null;
+        return this.vehicleRepository.save(vehicle);
     }
 
     @Override
     public void delete(String s) {
-
+        this.vehicleRepository.deleteById(s);
     }
 }
 
